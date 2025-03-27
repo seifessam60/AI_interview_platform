7155c259546f87f4c0f9e8db3133edbc909ce29d
seif essam <seifessam60@gmail.com>
2025-03-27 22:57:57 +0200

    Vapi Workflow (Endpoint)

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -4,7 +4,7 @@ const Page = () => {
   return (
     <>
       <h3>Interview Generation</h3>
-      <Agent userName={"user"} userId={"user1"} type={"generate"} />
+      <Agent userName={"user"} userId={"user1"} type={"vapi"} />
     </>
   );
 };
