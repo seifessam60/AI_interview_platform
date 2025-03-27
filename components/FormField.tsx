effc56d4cc4ba181cc3e783704b098de15c1ef0b
seif essam <seifessam60@gmail.com>
2025-03-27 23:24:51 +0200

    Vapi Workflow (Endpoint)

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,12 +1,6 @@
 import React from "react";
-import {
-  FormControl,
-  FormDescription,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { FormControl, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
 import { Control, Controller, FieldValues, Path } from "react-hook-form";
 
 interface FormFieldProps<T extends FieldValues> {
